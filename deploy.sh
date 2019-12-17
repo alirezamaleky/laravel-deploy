d710349f52513936888eb9e02d1d013f5858d896
Alireza Maleky <alirezaabdalmaleky@gmail.com>
2019-12-17 03:32:53 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -202,8 +202,8 @@ _up() {
     docker-compose up -d $CONTAINERS
 
     if [[ $INSTALL == "y" ]]; then
+        SQL_RUNNER="docker-compose exec $DB_ENGINE"
         if [[ $($SQL_RUNNER "mysql -u root -e -p$MYSQL_ROOT_PASSWORD 'SHOW DATABASES;'") == *"ERROR"* ]] || [[ $($SQL_RUNNER "mysql -u $DB_USERNAME -e -p$DB_PASSWORD 'SHOW DATABASES;'") == *"ERROR"* ]]; then
-            SQL_RUNNER="docker-compose exec $DB_ENGINE"
 
             if [[ $($SQL_RUNNER "mysql -u root -e 'SHOW DATABASES;'") != *"ERROR"* ]]; then
                 SQL="mysql -u root"
@@ -216,10 +216,10 @@ _up() {
             fi
 
             $SQL_RUNNER \
-                $SQL -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY $DB_ROOT_PASSWORD;" &&
+                $SQL -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';" &&
                 $SQL -e "CREATE DATABASE IF NOT EXISTS $DB_DATABASE COLLATE 'utf8_general_ci';" &&
-                $SQL -e "CREATE USER '$DB_USERNAME'@'%' IDENTIFIED WITH mysql_native_password BY '$DB_PASSWORD';" &&
-                $SQL -e "GRANT ALL ON $DB_DATABASE.* TO '$DB_USERNAME'@'%';"
+                $SQL -e "CREATE USER '$DB_USERNAME'@'localhost' IDENTIFIED BY '$DB_PASSWORD';" &&
+                $SQL -e "GRANT ALL ON $DB_DATABASE.* TO '$DB_USERNAME'@'localhost';"
         fi
     fi
 
