6e3c6309edd100963d1591bcf8434c24dfdbf2a9
Alireza Maleky <alirezaabdalmaleky@gmail.com>
2019-12-23 11:38:10 +0330

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -179,7 +179,7 @@ _env() {
         if ! grep -q "$LARADOCK_PATH && docker-compose up -d" /etc/crontab; then
             sudo echo "@reboot root  cd $LARADOCK_PATH && docker-compose up -d $CONTAINERS" >>/etc/crontab
         else
-            sed -i "$LARADOCK_PATH && docker-compose up -d*|$LARADOCK_PATH && docker-compose up -d $CONTAINERS|" /etc/crontab
+            sed -i "s|$LARADOCK_PATH && docker-compose up -d*|$LARADOCK_PATH && docker-compose up -d $CONTAINERS|" /etc/crontab
         fi
 
         if ! grep -q "$SCRIPT_PATH deploy" /etc/crontab; then
